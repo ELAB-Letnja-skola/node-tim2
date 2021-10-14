cde1af323a75275221e53a67d39ca596a2469f6d
unknown <vojaxste98@gmail.com>
2021-10-14 20:42:43 +0200

    treci deo gotov

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 4000;
 // TODO - Write a get request using path "/second" that will return response with the status code 200 and json message 'This is a second request.'"
 app.get("/second", (req, res) => { res.status(200).json("This is a second request."); });
 // TODO - Write a get request using path "/third" that will return response with the status code 200 and json message 'This is a third request.'"
-
+app.get("/third", (req, res) => { res.status(200).json("This is a third request."); });
 
 
 module.exports = app.listen(port, () =>
